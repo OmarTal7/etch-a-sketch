1f5a382ab8eacfc1a7612f5e6f1ce46f79eaf232
Omar <85651020+OmarTal7@users.noreply.github.com>
2023-12-11 06:43:32 -0500

    added event listeners to handle drawing only when mousedown, and stop drawing on mouseup

--- logic.js
+++ logic.js
@@ -4,24 +4,37 @@ let height = `${500/number}px`;
 let width = `${500/number}px`;
 
 
+// Generate correct number of squares for etch-a-sketch container
 let i=0;
 while(i < (number**2)){
     const div = document.createElement('div');
     div.classList.add('pixel');
-    // div.textContent = "pixel";
     container.appendChild(div);
     i++;
 };
 
+
+// Setting height and width attributes
 const pixels = document.querySelectorAll('.pixel');
 pixels.forEach((pixel) => {
     pixel.setAttribute('style', 'height:' + height + '; width: ' + width + ';');
 });
 
 
+function changeColor(e){
+    e.target.style.background = 'black';
+};
 
 pixels.forEach((pixel) => {
-    pixel.addEventListener('mouseover', (e) =>{
-        e.target.style.background = 'black';
+    pixel.addEventListener('mousedown', () => {
+        pixels.forEach((pixel) => {
+            pixel.addEventListener('mouseover', changeColor);
+        })
     });
-});
+
+    pixel.addEventListener('mouseup', () => {
+        pixels.forEach((pixel) => {
+            pixel.removeEventListener('mouseover', changeColor);
+        })
+    });
+})
