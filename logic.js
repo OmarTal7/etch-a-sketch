57b233e7612eff23b1b6571ff676849620492d2f
Omar <85651020+OmarTal7@users.noreply.github.com>
2023-12-12 07:03:06 -0500

    added color randomizer function for rainbow mode and separated out function for grid event listeners

--- logic.js
+++ logic.js
@@ -1,12 +1,13 @@
 const container = document.querySelector('.container');
-const button = document.querySelector('button');
+const button = document.querySelector('#refresh-button');
 let number = 16;
 
 
 function promptUser(){
     let userNumber = prompt("What board size do you want? (e.g. type '3' for a '3x3', or '50' for a '50x50' grid)");
     return userNumber;
-}
+};
+
 
 // Generate correct number of squares for etch-a-sketch container
 function makeGrid(size){
@@ -28,25 +29,32 @@ function makeGrid(size){
     pixel.setAttribute('style', 'height:' + height + '; width: ' + width + ';');
         });
 
-    // Event listenenrs
-    pixels.forEach((pixel) => {
-    pixel.addEventListener('mousedown', () => {
-        pixels.forEach((pixel) => {
-            pixel.addEventListener('mouseover', changeColor);
-        })
-    });
-
-    pixel.addEventListener('mouseup', () => {
-        pixels.forEach((pixel) => {
-            pixel.removeEventListener('mouseover', changeColor);
-        })
-    });
-});
+    addEventListeners(pixels);
 };
 
+function addEventListeners(pixels){
+    pixels.forEach((pixel) => {
+        pixel.addEventListener('mousedown', () => {
+            pixels.forEach((pixel) => {
+                pixel.addEventListener('mouseover', changeRandomColor);
+            })
+        });
+    
+        pixel.addEventListener('mouseup', () => {
+            pixels.forEach((pixel) => {
+                pixel.removeEventListener('mouseover', changeRandomColor);
+            })
+        });
+    });
+};
 
 function changeColor(e){
     e.target.style.background = 'black';
+};
+
+function changeRandomColor(e){
+    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    e.target.style.background = randomColor;
 };
 
 makeGrid(number);
