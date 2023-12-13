a82b069968cab00f10ea6de67a5e836906e10435
Omar <85651020+OmarTal7@users.noreply.github.com>
2023-12-13 05:25:29 -0500

    re-wrote makeGrid function and first iteration of blackEventListeners implementation

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.container');
 const button = document.querySelector('#refresh-button');
 const rainbowMode = document.querySelector('#rainbow-mode');
 let number = 16;
-
+let gridParts = []; 
 
 
 function promptUser(){
@@ -10,82 +10,56 @@ function promptUser(){
     return userNumber;
 };
 
-
 // Generate correct number of squares for etch-a-sketch container
-function makeGrid(size, randomColors){
+function makeGrid(size){
 
-    let height = `${950/size}px`;
-    let width = `${950/size}px`;
+    let height = `${600/size}px`;
+    let width = `${600/size}px`;
     let i=0;
 
     while(i < (size**2)){
         const div = document.createElement('div');
         div.classList.add('pixel');
+        div.setAttribute('style', 'height:' + height + '; width: ' + width + ';');
         container.appendChild(div);
         i++;
     };
 
-    // Setting height/width attributes for grid elements
-    const pixels = document.querySelectorAll('.pixel');
-    pixels.forEach((pixel) => {
-    pixel.setAttribute('style', 'height:' + height + '; width: ' + width + ';');
-        });
-
-    (!randomColors) ? addCustomEventListeners(pixels, false) : addCustomEventListeners(pixels, true);
+    gridParts = document.querySelectorAll('.pixel');
 };
 
-function addCustomEventListeners(pixels, randomColors){
-    pixels.forEach((pixel) => {
-        pixel.addEventListener('mousedown', () => {
-            pixels.forEach((pixel) => {
-                (randomColors == false) ? pixel.addEventListener('mouseover', changeColor) : pixel.addEventListener('mouseover', changeRandomColor);
-            })
-        });
-    
-        pixel.addEventListener('mouseup', () => {
-            pixels.forEach((pixel) => {
-                (randomColors == false) ? pixel.removeEventListener('mouseover', changeColor) : pixel.removeEventListener('mouseover', changeRandomColor);
-            })
-        });
+function colorBlack(){
+    gridParts.forEach((part) => {
+        part.addEventListener('mouseover', () =>{
+            part.style.background = 'black';
+        })
     });
 };
 
-function changeColor(e){
-    e.target.style.background = 'black';
+function drawBlack(){
+    gridParts.forEach((part) =>{
+        part.addEventListener('mousedown', colorBlack);
+
+        part.addEventListener('mouseup', ()=> {
+            part.removeEventListener('mousedown', colorBlack);
+        })
+    });
 };
 
-function changeRandomColor(e){
-    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    e.target.style.background = randomColor;
-};
 
-function toggleButton(button){
-    if (button.value == "ON"){
-        button.value = "OFF";
-    } else if (button.value == "OFF"){
-        button.value = "ON";
-    }
-};
+makeGrid(40);
+drawBlack();
 
-makeGrid(number);
 
-button.addEventListener('click', () =>{
-    makeGrid(promptUser());
-});
 
-rainbowMode.addEventListener('click', () =>{
-    let pixels = document.querySelectorAll('.pixel');
 
-    if(rainbowMode.value == "OFF"){
-        toggleButton(rainbowMode);
-        console.log(rainbowMode.value);
-        addCustomEventListeners(pixels, true);
-    } else if(rainbowMode.value == "ON"){
-        toggleButton(rainbowMode);
-        console.log(rainbowMode.value);
-        addCustomEventListeners(pixels, false);
-    }
-});
+
+
+
+// function changeRandomColor(e){
+//     let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+//     e.target.style.background = randomColor;
+// };
 
 
 
